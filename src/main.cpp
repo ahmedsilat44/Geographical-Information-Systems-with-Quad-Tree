29a20c285dad957ce15189d5f703080ba7c881aa
ahmedsilat44 <ahmedsilat44@gmail.com>
2025-04-14 22:44:29 +0500

    fixed visualisation

--- src/main.cpp
+++ src/main.cpp
@@ -11,9 +11,10 @@ namespace plt = matplotlibcpp;
 
 int main() {
     srand(time(0)); // Seed for random number generation
-
+    int limit = 20;
+    Point centre (10, 10); // Center of the Quadtree
     // Define the boundary of the Quadtree (center at (0,0), width and height of 20)
-    Box boundary(Point(10, 10), 20, 20);
+    Box boundary(centre, limit, limit);
     Quadtree qt(boundary, 4); // Max capacity of 4 points per node
 
     // Insert 10 random points into the Quadtree
@@ -27,11 +28,11 @@ int main() {
 
     //     std::cout << "Inserted: (" << x << ", " << y << ")\n";
     // }
-    plt::xlim(0, 20);
-    plt::ylim(0, 20);
-    plt::title("Quadtree Points Insertion");
-    plt::xlabel("X-axis");
-    plt::ylabel("Y-axis");
+    // plt::xlim(0, limit);
+    // plt::ylim(0, limit);
+    // plt::title("Quadtree Points Insertion");
+    // plt::xlabel("X-axis");
+    // plt::ylabel("Y-axis");
 
     Point p1(15.98, 8.21);
     qt.insert_point(p1);
@@ -64,11 +65,40 @@ int main() {
     Point p13(8, 8);
     qt.insert_point(p13);
 
-    Point p14(20, 7);
+    Point p14(19.9, 7);
     qt.insert_point(p14);
+    Point p15(17.17, 14.14);
+    Point p16(1.11, 19.19);
+    Point p17(18.81, 9.09);
+    Point p18(14.14, 3.33);
+    Point p19(3.33, 15.15);
+    Point p20(6.06, 7.07);
+    Point p21(9.99, 16.16);
+    Point p22(11.22, 2.22);
+    Point p23(7.89, 11.11);
+    Point p24(19.5, 5.55);
+    Point p25(2.22, 18.18);
+    Point p26(2.5, 17.5);
+    Point p27(3.5, 17.5);
+    Point p28(3.5, 15.5);
+    // qt.insert_point(p15);
+    // qt.insert_point(p16);
+    // qt.insert_point(p17);
+    // qt.insert_point(p18);
+    // qt.insert_point(p19);
+    // qt.insert_point(p20);
+    // qt.insert_point(p21);
+    // qt.insert_point(p22);
+    // qt.insert_point(p23);
+    // qt.insert_point(p24);
+    // qt.insert_point(p25);
+    // qt.insert_point(p26);
+    // qt.insert_point(p27);
+    // qt.insert_point(p28);
+
     // Print the Quadtree structure
-    std::cout<< "\nQuadtree Structure:\n";
-    qt.print_Quadtree();
+    // std::cout<< "\nQuadtree Structure:\n";
+    // qt.print_Quadtree();
 
     // Extract points from the Quadtree for plotting
     std::vector<Point> points = qt.get_points();
@@ -76,7 +106,7 @@ int main() {
     x_vals.push_back(points[0].get_x());
     y_vals.push_back(points[0].get_y());
     
-    plt::scatter(x_vals, y_vals, 10.0, {{"color", "k"}});
+    // plt::scatter(x_vals, y_vals, 10.0, {{"color", "k"}});
     
     // for (auto& point : points) {
     //     x_vals.push_back(point.get_x());
@@ -97,9 +127,9 @@ int main() {
     
     // call draw for each quadtree that is divided
     
-    plt::title("Quadtree Points");
+    // plt::title("Quadtree Points");
     
-    plt::show();
+    // plt::show();
 
     return 0;
 }
